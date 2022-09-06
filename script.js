230a5d7bbe3d9d4bc15114a06f304a93948d6fe2
ddhere <devanshdubey002@gmail.com>
2022-09-07 01:32:25 +0530

    V1.5

--- script.js
+++ script.js
@@ -1,16 +1,24 @@
-let touchstartX = 0
-let touchendX = 0
-    
-function checkDirection() {
-  if (touchendX < touchstartX) console.log('swiped left!')
-  if (touchendX > touchstartX) console.log('swiped right!')
+document.addEventListener('scroll', e => {
+  scrollstartY = document.documentElement.scrollTop
+  console.log(scrollstartY)
+  checkEnd(scrollstartY);
+})
+
+leaf = document.getElementById(leaf);
+q1 = document.getElementById(q1);
+lander = document.getElementById(lander);
+
+function checkEnd(scrollstartY) {
+    if (scrollstartY>28){
+        endFunction();
+        console.log("End is here");
+    }
 }
 
-document.addEventListener('touchstart', e => {
-  touchstartX = e.changedTouches[0].screenX
-})
-
-document.addEventListener('touchend', e => {
-  touchendX = e.changedTouches[0].screenX
-  checkDirection()
-})
+function endFunction(){
+    console.log("end was reached");
+    leaf.style.animation = "scrollend 2s 1";
+    leaf.style.width = "100%";
+    leaf.style.borderRadius = "1000px";
+    q1.style.display = "block";
+}
